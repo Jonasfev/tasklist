9fe9d4ab57cc5ba50428d388925fc8abf42d8972
Jonasfev <jonasvieirafelix@gmail.com>
2021-03-29 13:38:39 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,24 +1,47 @@
-<?php
-class Conexao{
-    private $host= "sql308.unaux.com";
-    private $dbname = "unaux_26273168_aulaBD";
-    private $user = "unaux_26273168";
-    private $senha = "sa21dr07";
-
-    public function conectar(){
-        try{
-            $conexao = new PDO("mysql:host=$this->host;dbname=$this->dbname","$this->user", "$this->senha");
-            return $conexao;
-        }
-        catch(PDOException $e){
-            echo '<br>Erro '. $e->getCode();
-            echo '<br>Mensagem '. $e->getMessage();
-            echo '<br>Arquivo '. $e->getFile();
-            echo '<br>Linha '. $e->getLine();
-        }
-    }
-
-}
-
-
+<?php
+
+class Conexao{
+
+    private $host= "";
+
+    private $dbname = "";
+
+    private $user = "u";
+
+    private $senha = "";
+
+
+
+    public function conectar(){
+
+        try{
+
+            $conexao = new PDO("mysql:host=$this->host;dbname=$this->dbname","$this->user", "$this->senha");
+
+            return $conexao;
+
+        }
+
+        catch(PDOException $e){
+
+            echo '<br>Erro '. $e->getCode();
+
+            echo '<br>Mensagem '. $e->getMessage();
+
+            echo '<br>Arquivo '. $e->getFile();
+
+            echo '<br>Linha '. $e->getLine();
+
+        }
+
+    }
+
+
+
+}
+
+
+
+
+
 ?>
